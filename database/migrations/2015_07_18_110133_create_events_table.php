e32c04b62ccc1450a7f40e1b67a2f7afcf345e6a
Atomsk1988 <braisva@hotmail.com>
2015-07-18 14:50:34 +0200

    initial commit

---
+++ database/migrations/2015_07_18_110133_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('desc')->nullable();
+            $table->time('start')->nullable();
+            $table->time('end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
---
+++ resources/views/users.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+
+@section('content')
+	User index:
+@stop

142f9551a79e658f018e79b40b8329ae325cc1f2
Atomsk1988 <braisva@hotmail.com>
2015-07-27 09:34:43 +0200

    Update loginForm.blade.php
    auth user

--- resources/views/partials/loginForm.blade.php
+++ resources/views/partials/loginForm.blade.php
@@ -1,4 +1,4 @@
-<!--
+@if(!$user = Auth::user())
 <form action="/auth/login" method="post" class="navbar-form navbar-right" role="form">
 	<div class="form-group">
 		<input type="text" placeholder="Email" class="form-control">
@@ -8,9 +8,8 @@
 	</div>
 	<button type="submit" class="btn btn-success">Sign in	</button>
 </form>
--->
-
+@else
 <div id="navbar" class="navbar-collapse collapse navbar-right">
-     <a href="./auth/login">Log in</a> <a href="./auth/register">Register{{ $logged }}</a>
+     <a href="./auth/login">Log in</a> <a href="./auth/register">Register{{ $user->name }}</a>
 </div>
-
+@endif
